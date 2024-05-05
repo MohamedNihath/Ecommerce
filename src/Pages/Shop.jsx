b34130a716af2bd3dcaecc7b4f4c94058e72cc78
MohamedNihath <mohamednihath0308@gmail.com>
2024-05-05 17:43:28 +0530

    complete a NewCollection

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Hero from '../Components/Hero/Hero'
 import Popular from '../Components/Popular/Popular'
 import Offers from '../Components/Offers/Offers'
+import NewCollection from '../Components/NewCollection/NewCollection'
 
 const shop = () => {
   return (
@@ -10,6 +11,7 @@ const shop = () => {
       <Hero/>
       <Popular/>
       <Offers/>
+      <NewCollection/>
     </div>
   )
 }
