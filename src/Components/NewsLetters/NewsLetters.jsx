04cd088e8e5adf5e8f6ef350b958aacb93a681f2
MohamedNihath <mohamednihath0308@gmail.com>
2024-05-05 17:48:21 +0530

    create a newsletter folder & files

---
+++ src/Components/NewsLetters/NewsLetters.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NewsLetters.css'
+
+const NewsLetters = () => {
+  return (
+    <div className='newsletters'>
+      
+    </div>
+  )
+}
+
+export default NewsLetters
